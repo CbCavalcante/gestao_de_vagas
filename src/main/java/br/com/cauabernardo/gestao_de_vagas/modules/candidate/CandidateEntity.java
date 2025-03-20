adfd1a7a7e52c8e23884fed8ebcaba5b8356fb0a
bernardo <admin@bernardo.com>
2025-03-20 19:33:01 -0400

    Implementation of validations, error exceptions that are more user-friendly

--- src/main/java/br/com/cauabernardo/gestao_de_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/cauabernardo/gestao_de_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,11 @@ package br.com.cauabernardo.gestao_de_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +14,15 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @NotBlank(message = "O campo [username] não pode estar em branco")
+    @Pattern(regexp = "^[^\\s]+$", message = "O campo [username] não deve conter espaços") // Validação com expressão regular
     private String username;
+
+    @Email(message = "O campo [email] deve conter um e-mail válido") // Validação
     private String email;
+
+    @Length(min = 8, max = 100, message = "A senha deve conter entre 8 e 100 caracteres") // Validação
     private String password;
     private String phone;
     private String description;
--- src/main/java/br/com/cauabernardo/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/cauabernardo/gestao_de_vagas/modules/candidate/controllers/CandidateController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cauabernardo.gestao_de_vagas.modules.candidate.CandidateEntity;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class CandidateController {
     
     @PostMapping("/")
-    public void create(@RequestBody CandidateEntity candidateEntity) {
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
         System.out.println("Candidato");
         System.out.println(candidateEntity.getEmail());
+        System.out.println(candidateEntity.getDescription());
+        System.out.println(candidateEntity.getUsername());
+        System.out.println(candidateEntity.getPassword());
     }
 }
